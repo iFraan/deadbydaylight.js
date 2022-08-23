d7dc926d2605b13f70a5aeabd03f352d792a9bf2
Francisco Zorat <franciscozorat@gmail.com>
2022-08-23 07:54:48 -0300

    rank names

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,25 @@
-
 const ranks = {
-    
+    3: 'Ash IV',
+    6: 'Ash III',
+    10: 'Ash II',
+    14: 'Ash I',
+    18: 'Bronze IV',
+    22: 'Bronze III',
+    26: 'Bronze II',
+    30: 'Bronze I',
+    35: 'Silver IV',
+    40: 'Silver III',
+    45: 'Silver II',
+    50: 'Silver I',
+    55: 'Gold IV',
+    60: 'Gold III',
+    65: 'Gold II',
+    70: 'Gold I',
+    75: 'Iridescent IV',
+    80: 'Iridescent III',
+    85: 'Iridescent II',
+    90: 'Iridescent I',
 }
-
 
 module.exports = {
     ranks
